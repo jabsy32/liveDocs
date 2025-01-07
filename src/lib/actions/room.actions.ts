2952c8ccd1114991f24f62bd8ea10868c46d158d
Paul Jabs <p.jabs2@newcastle.ac.uk>
2025-01-07 17:51:36 +0000

    add user notifications to show users who have been granted access and type

--- src/lib/actions/room.actions.ts
+++ src/lib/actions/room.actions.ts
@@ -100,7 +100,20 @@ export const updateDocumentAccess = async ({
     });
 
     if (room) {
-      //TODO: send notification to user
+      const notificationId = nanoid();
+
+      await liveblocks.triggerInboxNotification({
+        userId: email,
+        kind: "$documentAccess",
+        subjectId: notificationId,
+        activityData: {
+          userType,
+          title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+          updatedBy: updatedBy.name,
+          avatar: updatedBy.avatar,
+          email: updatedBy.email,
+        },
+      });
     }
 
     revalidatePath(`/documents/${roomId}`);
